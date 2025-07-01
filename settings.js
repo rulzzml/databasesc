cfc9e9548869f037c2d5f69fc396fa62a10cf6a8
rulzzml <139113956+rulzzml@users.noreply.github.com>
2025-07-01 11:36:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 // GitHub Configuration
 const GITHUB_CONFIG = {
-    owner: "RulzzOfficiaL",
-    repo: "RulzXD",
+    owner: "rulzztheking",
+    repo: "sc",
     path: "database.json",
     token: "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"
 };
 
 // Owner Configuration
 const OWNER_CONFIG = {
-    password: "RulzzOffc",
+    password: "Kntl",
     sessionTimeout: 30 * 60 * 1000 // 30 minutes
 };
