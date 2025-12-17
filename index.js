5a32006e39ffc469a98c3fe843aa699f12332a66
rulzzml <139113956+rulzzml@users.noreply.github.com>
2025-12-17 10:31:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
